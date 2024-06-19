9ac4a5a75567a86e5bf6aadaec1118c545f8c18f
Jenkibear <127636124+JenkinHe@users.noreply.github.com>
2024-06-19 17:36:34 +1000

    ploting data on a graph with plt

--- Magic.py
+++ Magic.py
@@ -2,11 +2,18 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-cols =["fLength","fWidth","fSize","fConc","fConci","fAsym","fM3Long","fM3Trans","fAlpha","fDist","class"]
+cols =["fLength","fWidth","fSize","fConc","fConc1","fAsym","fM3Long","fM3Trans","fAlpha","fDist","class"]
 df = pd.read_csv("magic04.data",names=cols)
 
 print(df.head())
 
 df["class"] =(df["class"]=="g").astype(int)
 
-
+for label in cols[:-1]:
+    plt.hist(df[df["class"]==1][label],color='blue',label='gamma',alpha=0.7,density=True)
+    plt.hist(df[df["class"]==0][label],color='red',label='gamma',alpha=0.7,density=True)
+    plt.title(label)
+    plt.ylabel("Probability")
+    plt.xlabel(label)
+    plt.legend()
+    plt.show()
