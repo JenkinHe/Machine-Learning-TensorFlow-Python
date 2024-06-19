a9fdbe74080546618f8b7ca96d4961a5ae0faeb4
Jenkibear <127636124+JenkinHe@users.noreply.github.com>
2024-06-19 18:33:44 +1000

    install over sampler packagae

--- Magic.py
+++ Magic.py
@@ -2,6 +2,7 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
+from imblearn.over_sampling import RandomOverSampler
 
 cols =["fLength","fWidth","fSize","fConc","fConc1","fAsym","fM3Long","fM3Trans","fAlpha","fDist","class"]
 df = pd.read_csv("magic04.data",names=cols)
@@ -10,14 +11,14 @@ print(df.head())
 
 df["class"] =(df["class"]=="g").astype(int)
 
-for label in cols[:-1]:
-    plt.hist(df[df["class"]==1][label],color='blue',label='gamma',alpha=0.7,density=True)
-    plt.hist(df[df["class"]==0][label],color='red',label='hadron',alpha=0.7,density=True)
-    plt.title(label)
-    plt.ylabel("Probability")
-    plt.xlabel(label)
-    plt.legend()
-    plt.show()
+# for label in cols[:-1]:
+#     plt.hist(df[df["class"]==1][label],color='blue',label='gamma',alpha=0.7,density=True)
+#     plt.hist(df[df["class"]==0][label],color='red',label='hadron',alpha=0.7,density=True)
+#     plt.title(label)
+#     plt.ylabel("Probability")
+#     plt.xlabel(label)
+#     plt.legend()
+#     plt.show()
 
 # Train, validation , test datasets
 
@@ -37,3 +38,5 @@ def scale_dataset(dataframe):
 
     return data,x,y
 
+print(len(train[train["class"]==1]))#gamma
+print(len(train[train["class"]==0]))#gamma
