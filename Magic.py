285d486e82a7c6408105f81c93a871b983a9072e
Jenkibear <127636124+JenkinHe@users.noreply.github.com>
2024-06-19 17:00:53 +1000

    matplotlib and display dataset with pandas

--- Magic.py
+++ Magic.py
@@ -1,3 +1,12 @@
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+
+cols =["fLength","fWidth","fSize","fConc","fConci","fAsym","fM3Long","fM3Trans","fAlpha","fDist","class"]
+df = pd.read_csv("magic04.data",names=cols)
+
+print(df.head())
+
+df["class"] =(df["class"]=="g").astype(int)
+
+
