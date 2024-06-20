3be80aa4d99e68b76348924b86212a4d3a7f3fb4
Jenkibear <127636124+JenkinHe@users.noreply.github.com>
2024-06-20 18:22:20 +1000

    datasets sorted into train validation and test sets with temp x and bike count y

--- Bikes.py
+++ Bikes.py
@@ -29,3 +29,28 @@ df= df.drop(["hour"],axis=1)
 
 df=df.drop(["wind","visibility","functional"],axis=1)
 print(df.head())
+
+# Train/valid/test dataset
+
+train,val,test=np.split(df.sample(frac=1), [int(0.6*len(df)),int(0.8*len(df))])
+
+def get_xy(dataframe,y_label, x_label=None):
+    dataframe=copy.deepcopy(dataframe)
+    if not x_label:
+        x= dataframe[[c for c in dataframe.columns if c!=y_label]].values
+    else:
+        if len(x_label)==1:
+            x= dataframe[x_label[0]].values.reshape(-1,1)
+        else:
+            x=dataframe[x_label].values
+    
+    y=dataframe[y_label].values.reshape(-1,1)
+    data= np.hstack((x,y))
+
+    return data,x,y
+
+_, x_train_temp,y_train_temp=get_xy(train,"bike_count",x_label=["temp"])
+_, x_val_temp,y_val_temp=get_xy(val,"bike_count",x_label=["temp"])
+_, x_test_temp,y_test_temp=get_xy(test,"bike_count",x_label=["temp"])
+
+
