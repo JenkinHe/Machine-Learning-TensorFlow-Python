40e33a9c692b438dbe1b2a4f978e177c0c760f01
Jenkibear <127636124+JenkinHe@users.noreply.github.com>
2024-06-20 14:52:18 +1000

    logistirc regression and support vector machines

--- Magic.py
+++ Magic.py
@@ -6,6 +6,8 @@ from imblearn.over_sampling import RandomOverSampler
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import classification_report
 from sklearn.naive_bayes import GaussianNB
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
 
 cols =["fLength","fWidth","fSize","fConc","fConc1","fAsym","fM3Long","fM3Trans","fAlpha","fDist","class"]
 df = pd.read_csv("magic04.data",names=cols)
@@ -64,6 +66,21 @@ nb_model=nb_model.fit(x_train,y_train)
 
 y_pred =nb_model.predict(x_test)
 
-print(classification_report(y_test,y_pred))
+# print(classification_report(y_test,y_pred))
 
 # Logistic regression
+
+lg_model=LogisticRegression()
+lg_model= lg_model.fit(x_train, y_train)
+y_pred =lg_model.predict(x_test)
+
+# print(classification_report(y_test,y_pred))
+
+# Support vector machine //not great with outliers
+
+svm_model= SVC()
+svm_model = svm_model.fit(x_train,y_train)
+
+y_pred =svm_model.predict(x_test)
+
+print(classification_report(y_test,y_pred))
